c814b283f87d1a3b1453c4f111c16a93d82bece6
ElyssonHiago <99830595+ElyssonHiago@users.noreply.github.com>
2024-10-21 10:14:01 -0300

    .

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask 
+from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
@@ -6,6 +8,19 @@ app = Flask(__name__)
 def index():
     return "<h1>Olá <br> Mundo!</h1>"
 
-@app.route("/aluno/<nome>")
-def aluno(nome):
-    return nome
+@app.route("/aluno")
+def aluno():
+    return render_template("formulario.html")
+
+@app.route("/envio", methods=['POST'])
+def envioForms():
+    name = request.form["nome"]
+    senha = request.form['senha']
+
+    if senha == '123':
+        mensagem = "você tá logado, bestie!"
+
+    else:
+        mensagem = "senha incorreta, tente novamente!"
+
+    return render_template('resposta.html', m = mensagem)
